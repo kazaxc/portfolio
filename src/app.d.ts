7d0327d47aec9598bd825cd83daa1a587947eabd
kazaxc <lgkscast@gmail.com>
2025-11-24 20:06:33 +0000

    Added PrelineUI to the site for styled components

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+import { IStaticMethods } from "preline/dist";
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -8,6 +10,20 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+
+	interface Window {
+    // Optional plugins
+    _;
+    $: typeof import("jquery");
+    jQuery: typeof import("jquery");
+    DataTable;
+	noUiSlider: typeof import("nouislider");
+    Dropzone;
+    VanillaCalendarPro;
+
+    // Preline UI
+    HSStaticMethods: IStaticMethods;
+  }
 }
 
 export {};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { afterNavigate } from "$app/navigation";
 	
 	let { children } = $props();
+
+	afterNavigate(() => {
+    window.HSStaticMethods.autoInit();
+  	});
 </script>
 
 <svelte:head>
